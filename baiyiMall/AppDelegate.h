83a6014f9026813ed84f0b95803001bd66f32882
Dv_wang <dv_wang@Dv-wangdeiMac.local>
2015-10-04 20:39:54 +0800

    Initial Commit

---
+++ baiyiMall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  baiyiMall
+//
+//  Created by Dv_wang on 15-10-4.
+//  Copyright (c) 2015年 Dv_wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ baiyiMall/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  baiyiMall
+//
+//  Created by Dv_wang on 15-10-4.
+//  Copyright (c) 2015年 Dv_wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
